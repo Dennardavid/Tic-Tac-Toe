b9dec40ac8e4c9a0cf86247fc361762568cbf94b
dennar david <dennardavid@gmail.com>
2023-10-25 21:40:20 +0100

    click on keys to show player mark added

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,4 +1,6 @@
 /* Variables used */
+const buttonX = document.querySelector("#image-x");
+const buttonO = document.querySelector("#image-o");
 const play_against_cpu = document.querySelector(".play_against_cpu");
 const play_against_person = document.querySelector(".play_against_person");
 const play_game = document.querySelector(".play_game");
@@ -10,24 +12,110 @@ const modal_button_1 = document.querySelector(".modal_button_1");
 const modal_button_2 = document.querySelector(".modal_button_2");
 const modal_pic = document.querySelector(".modal_pic");
 const modal_header = document.querySelector(".modal_header");
-const imageX = document.querySelector("#image-x");
-const imageO = document.querySelector("#image-o");
+const hidden = document.querySelector(".hidden");
+const gameButtons = document.querySelectorAll(".keys");
+let playerSymbol = "";
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+// Set the player to X
+buttonX.addEventListener("click", function () {
+  /* Set current player to X */
+  playerSymbol = "X";
+
+  // Set the background so that two backgrounds can't be the same at once when selecting a player
+  buttonX.style.backgroundColor = "#60b347";
+  buttonO.style.backgroundColor = "#a8bfc9";
+
+  // Making selection of player area hidden and actual game visible for cpu works only after selecting your player mark
+  playCpu();
+  playPerson();
+
+  /* Function call to restart game */
+  restartModal();
+  restart();
+
+  modal.addEventListener("click", closeModal);
+
+  /* Fuction call on cancel button */
+  modal_button_1.addEventListener("click", closeModal);
+
+  /* Function call to displayer player image on the keys */
+  addClickListenersToButtons();
+});
+
+buttonO.addEventListener("click", function () {
+  playerSymbol = "O";
+
+  // Set the background so that two backgrounds can't be the same at once when selecting a player
+  buttonO.style.backgroundColor = "#60b347";
+  buttonX.style.backgroundColor = "transparent";
+
+  // Making selection of player area hidden and actual game visible for cpu works only after selecting your player mark
+  playCpu();
+  playPerson();
+
+  /* Function call to restart game */
+  restartModal();
+  restart();
+
+  /* Function calls to close the modal */
+  modal.addEventListener("click", closeModal);
+
+  /* Fuction call on cancel button */
+  modal_button_1.addEventListener("click", closeModal);
+
+  /* Function call to displayer player image on the keys */
+  addClickListenersToButtons();
+});
 
 /* Funtion to switch to playing pc mode */
 const playCpu = function () {
   play_against_cpu.addEventListener("click", function () {
-    play_game.style.display = "block";
-    pickplayer.style.display = "none";
+    if (playerSymbol === "") {
+      console.log("no player selected");
+    } else {
+      play_game.style.display = "block";
+      pickplayer.style.display = "none";
+    }
+    let cpuMove = Math.trunc(Math.random() * 9);
+    console.log(cpuMove);
   });
 };
 
 /* Funtion to switch to playing with another player mode */
 const playPerson = function () {
   play_against_person.addEventListener("click", function () {
-    play_game.style.display = "block";
-    pickplayer.style.display = "none";
+    if (playerSymbol === "") {
+      console.log("no player selected");
+    } else {
+      play_game.style.display = "block";
+      pickplayer.style.display = "none";
+    }
   });
 };
+
+/* Function to listen to all the buttons on the keypad */
+function addClickListenersToButtons() {
+  gameButtons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+      let buttonImage = button.querySelector(".gameimg");
+      if (buttonImage.getAttribute("src") === "") {
+        buttonImage.setAttribute(
+          "src",
+          playerSymbol === "X" ? "./assets/icon-x.png" : "./assets/icon-o.png"
+        );
+        buttonImage.setAttribute(
+          "alt",
+          playerSymbol === "X" ? "Player X" : "Player O"
+        );
+      }
+    });
+  });
+}
 
 /* Function to bring up restart modal */
 const restartModal = function () {
@@ -44,68 +132,24 @@ const restartModal = function () {
   });
 };
 
-/* Function to close modal with escape key */
-const closeModal = function () {
-  modal.style.visibility = "hidden";
-};
-
 /* Function to restart the game */
 const restart = function () {
   modal_button_2.addEventListener("click", function () {
     modal.style.visibility = "hidden";
     play_game.style.display = "none";
     pickplayer.style.display = "block";
-    imageX.style.backgroundColor = "transparent";
-    imageO.style.backgroundColor = "#a8bfc9";
+    buttonX.style.backgroundColor = "transparent";
+    buttonO.style.backgroundColor = "#a8bfc9";
+    playerSymbol = "";
+    gameButtons.forEach(function (button) {
+      let buttonImage = button.querySelector(".gameimg");
+      buttonImage.setAttribute("src", playerSymbol === "X" ? "" : "");
+      buttonImage.setAttribute("alt", playerSymbol === "X" ? "" : "");
+    });
   });
 };
 
-// Function for when player is X
-imageX.addEventListener("focus", function () {
-  // Set the player to X
-  const playerMark = "X";
-
-  // Set the background so that two backgrounds can't be the same at once when selecting a player
-  imageX.style.backgroundColor = "#60b347";
-  imageO.style.backgroundColor = "#a8bfc9";
-
-  // Making selection of player area hidden and actual game visible for cpu works only after selecting your player mark
-  playCpu();
-  playPerson();
-
-  /* Function call to restart game */
-  restartModal();
-
-  restart();
-
-  /* Function calls to close the modal */
-  modal.addEventListener("click", closeModal);
-
-  /* Fuction call on cancel button */
-  modal_button_1.addEventListener("click", closeModal);
-});
-
-// Function for when player is O
-imageO.addEventListener("focus", function () {
-  // Set the player to O
-  const playerMark = "O";
-
-  // Set the background so that two backgrounds can't be the same at once when selecting a player
-  imageO.style.backgroundColor = "#60b347";
-  imageX.style.backgroundColor = "transparent";
-
-  // Making selection of player area hidden and actual game visible for cpu works only after selecting your player mark
-  playCpu();
-  playPerson();
-
-  /* Function call to restart game */
-  restartModal();
-
-  restart();
-
-  /* Function calls to close the modal */
-  modal.addEventListener("click", closeModal);
-
-  /* Fuction call on cancel button */
-  modal_button_1.addEventListener("click", closeModal);
-});
+/* Function to close modal with escape key */
+const closeModal = function () {
+  modal.style.visibility = "hidden";
+};
